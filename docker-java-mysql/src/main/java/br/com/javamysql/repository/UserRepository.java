b1e3f5cba720c976314da66d962c4c413ca17f06
Leonardo Gaspar Saheb <leosaheb2003@gmail.com>
2024-08-16 23:02:11 -0300

    update

--- docker-java-mysql/src/main/java/br/com/javamysql/repository/UserRepository.java
+++ docker-java-mysql/src/main/java/br/com/javamysql/repository/UserRepository.java
@@ -2,5 +2,12 @@ package br.com.javamysql.repository;
 
 import br.com.javamysql.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("from Users where firstName = : firstName")
+    public user findByFirstName(@Param("firstName")){
+
+    }
+}
